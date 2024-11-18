89cad341d27e8446a91fd6f7ba5e1d66e62aca32
Mohammad Saquib <129363732+saquibe@users.noreply.github.com>
2024-11-18 22:30:10 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,7 +3,7 @@ export const educations = [
     id: 1,
     title: "Master of Computer Application",
     duration: "Aug 2022 - Jun 2024",
-    institution: "Kamla Nehru Institute of Technology Sultanpur",
+    institution: "Madan Mohan Malaviya University Of Technology",
   },
   {
     id: 2,
@@ -13,8 +13,8 @@ export const educations = [
   },
   {
     id: 3,
-    title: "Bachelor of Art - Geography",
-    duration: "Aug 2013 - Jun 2016",
-    institution: "Veer Bahadur Singh Purvanchal University Jaunpur",
+    title: "Intermediate",
+    duration: "Aug 2013 - Jun 2015",
+    institution: "Talimuddin Inter College Mau",
   },
 ]
