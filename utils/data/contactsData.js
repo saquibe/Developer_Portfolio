aa1a4328e4b1e8bf13008246389086712e16a5d6
Mohammad Saquib <129363732+saquibe@users.noreply.github.com>
2024-11-18 21:14:12 +0530

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'er.adilrasheed@gmail.com',
+    phone: '+919616316925',
+    address: 'Hyderabad, Telangana,India',
+    github: 'https://github.com/eradilrasheed',
+    facebook: 'https://www.facebook.com/Er.MARasheed/',
+    linkedIn: 'https://www.linkedin.com/in/madrashee/',
+    twitter: 'https://twitter.com/er_marasheed',
+    stackOverflow: 'https://stackoverflow.com/users/23922646/mohammad-adil-rasheed',
+    devUsername: "adil4474"
+}
